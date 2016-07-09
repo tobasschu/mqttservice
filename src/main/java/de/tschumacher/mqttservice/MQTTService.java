9cb38bb4301e1035f914fd61c24e88a9fa4a4cfd
Tobias Schumacher <tobias@tschumacher.de>
2016-07-09 13:21:33 +0200

    * set connection handler

--- src/main/java/de/tschumacher/mqttservice/MQTTService.java
+++ src/main/java/de/tschumacher/mqttservice/MQTTService.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2016 Tobias Schumacher
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -14,6 +14,7 @@
 package de.tschumacher.mqttservice;
 
 import de.tschumacher.mqttservice.consumer.MQTTMessageHandler;
+import de.tschumacher.mqttservice.handler.MQTTServiceConnectionHandler;
 import de.tschumacher.mqttservice.message.MQTTMessage;
 
 
@@ -28,6 +29,8 @@ public interface MQTTService<F> {
   void unsubscribe(String topic);
 
   void publish(String topic, MQTTMessage<F> message);
+
+  void setConnectionHandler(MQTTServiceConnectionHandler connectionHandler);
 
 
 
